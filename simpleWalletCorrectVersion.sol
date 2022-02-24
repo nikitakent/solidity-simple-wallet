7023ff73c244fbb5f428296e0f2d5ff1f96ee99f
Nikita Kent <nikita.kent@icloud.com>
2022-02-24 16:56:14 +1300

    Final changes
    SafeMath not implemented.
    RenounceOwnership not used (and therefore not overriden) because Ownable file only needs to be simple.... didn't put in all the fluff. 
    Added events to the withdraw and deposit (fallback) functions.

--- simpleWalletCorrectVersion.sol
+++ simpleWalletCorrectVersion.sol
@@ -1,36 +1,47 @@
 pragma solidity ^0.5.17;
 
 import "./Owned.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-contract SimpleWallet is Owned {
+contract Allowance is Owned {
+    event AllowanceChanged(address _forWho, address indexed _fromWhom, uint _oldAmount, uint newAmount);
 
-mapping (address => uint) public allowance; 
+    mapping (address => uint) public allowance; 
 
-function addAllowance(address _who, uint _amount) public onlyOwner{
-    allowance[_who] = _amount;
-}
-
-modifier ownerOrAllowed(uint _amount){
-    require(msg.sender == owner || allowance[msg.sender] >= _amount, "You do not have the required permissions to perform this operation.");
-    _;
-}
-
-function reduceAllowance(address _who, uint _amount) internal {
-    allowance[_who] -= _amount;
-}
-
-    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
-    // lowers allowance if the msg.sender is not the owner... why do this?
-    require(_amount <= address(this).balance, "There are not enough funds in the smart contract") // why use the (this) notation...?
-       if(msg.sender != owner){
-            reduceAllowance(msg.sender, _amount);
-       }
-        _to.transfer(_amount);
+    function addAllowance(address _who, uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
     }
 
+    modifier ownerOrAllowed(uint _amount){
+        require(msg.sender == owner || allowance[msg.sender] >= _amount, "You do not have the required permissions to perform this operation.");
+        _;
+    }
 
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] =- (_amount);
+    }
+}
+
+contract SimpleWallet is Allowance {
+
+    event MoneyRecieved(address indexed _from, uint _amount);
+    event MoneySent(address indexed _beneficiary, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+    // lowers allowance if the msg.sender is not the owner... why do this? 
+    // owner can override allowance amount. Do we really want this?
+        require(_amount <= address(this).balance, "There are not enough funds in the smart contract"); // why use the (this) notation...?
+       if(msg.sender != owner){
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
+        _to.transfer(_amount);
+    }
 // FALL BACK FUNCTION
     function () external payable {
+        emit MoneyRecieved(msg.sender, msg.value);
     }
 
 }
